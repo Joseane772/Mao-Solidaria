24a017c71da73ef0e9ed17a1b257472bc160e28f
tiagooliveira95 <t.oliveira.95@gmail.com>
2024-05-22 23:42:30 +0100

    points deduction logic

--- js/brindes.js
+++ js/brindes.js
@@ -4,7 +4,47 @@ document.addEventListener('DOMContentLoaded', function() {
     links.forEach(link => {
         link.addEventListener('click', function() {
             const clickedId = this.id;
-            alert('link with id ' + clickedId);
+
+            switch (clickedId) {
+                case '1':
+                    if (deductPoints(1000)) {
+                        alert('T-shirt Brinde sera enviado para a sua morada');
+                    }else {
+                        alert('Pontos insuficientes');
+                    }
+                    break;
+                case '2':
+                    if (deductPoints(500)) {
+                        alert('Chavena Brinde sera enviado para a sua morada');
+                    }else {
+                        alert('Pontos insuficientes');
+                    }
+                    break;
+                case '3':
+                    if (deductPoints(1500)) {
+                        alert('Caneta Brinde sera enviado para a sua morada');
+                    }else {
+                        alert('Pontos insuficientes');
+                    }
+                    break;
+                default:
+                    break;
+            }
         });
     });
 });
+
+function deductPoints(points) {
+    // Get the current points from local storage
+    let currentPoints = localStorage.getItem('points');
+
+    if (currentPoints < points) {
+        return false;
+    }
+
+    // Deduct the points
+    currentPoints -= points;
+    localStorage.setItem('points', currentPoints);
+
+   return true
+}
