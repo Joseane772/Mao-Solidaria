1e48c0571c6f8b8ce18f797d0e39c0ba68a5e669
tiagooliveira95 <t.oliveira.95@gmail.com>
2024-05-23 00:26:24 +0100

    reset points when at zero

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    if (!localStorage.getItem('points')) {
+    if (!localStorage.getItem('points') || localStorage.getItem('points') === '0'){
         localStorage.setItem('points', 5000); // Set the initial points to 5000
     }
 });
